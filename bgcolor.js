d23133aa62037ef20159a23ade092d7cf71bdf74
kacieel <41460966+oupi1234@users.noreply.github.com>
2018-12-26 20:22:06 +0900

    Add files via upload

---
+++ bgcolor.js
@@ -0,0 +1,34 @@
+var Link = {
+  setColor:function(color){
+    var alist = document.queryselectorAll('a');
+    var i = 0;
+     while(i < alist.lenght){
+       alist[i].style.color = color;
+       i = i + 1;
+     }
+  }
+}
+var Body = {
+  setColor:function(color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function (color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if (self.value ==='night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white')
+    self.value = 'day';
+
+    Link.setColor('powerblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black')
+    self.value = 'night';
+
+     Link.setColor('blue');
+  }
+}
